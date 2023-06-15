c87cadd421c2d32bd06466a41725466bbf4fc28a
Marina Barcelo <marinadevops@gmail.com>
2023-06-15 21:22:57 +0200

    Proyecto carouselPosts

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strinct";
+
+let CHECKED = false;
+
+document.addEventListener("pointerdown", (e) => {
+    CHECKED = !CHECKED;
+    document.documentElement.style.setProperty("--light", CHECKED ? 1:0);
+});
